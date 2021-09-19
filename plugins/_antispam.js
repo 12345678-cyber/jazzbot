3aa20550fea289fe923577270e3eb3f8c56eb67a
rishabh-modi2 <88582334+rishabh-modi2@users.noreply.github.com>
2021-09-19 09:22:47 +0530

    Update _antispam.js

--- plugins/_antispam.js
+++ plugins/_antispam.js
@@ -6,9 +6,9 @@ handler.all = async function (m) {
     if (m.sender in this.spam) {
         this.spam[m.sender].count++
         if (m.messageTimestamp.toNumber() - this.spam[m.sender].lastspam > 10) {
-            if (this.spam[m.sender].count > 10) {
+            if (this.spam[m.sender].count > 100) {
                 //global.db.data.users[m.sender].banned = true
-                m.reply('*Jangan Spam!!*')
+                m.reply('*do not spam, msg sent by jarvis!!*')
             }
             this.spam[m.sender].count = 0
             this.spam[m.sender].lastspam = m.messageTimestamp.toNumber()
